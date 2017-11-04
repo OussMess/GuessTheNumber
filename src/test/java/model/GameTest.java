01a24641cd48813957d424c526858c5c3cc02a85
oussmess <o.messaoudi@outlook.com>
2017-11-04 17:13:36 +0000

    Add tests

---
+++ src/test/java/model/GameTest.java
@@ -0,0 +1,27 @@
+package model;
+
+import org.junit.Test;
+
+/* Created by Oussama on 04/11/2017. */
+public class GameTest {
+    @Test
+    public void setPlayers() throws Exception {
+    }
+
+    @Test
+    public void attach() throws Exception {
+    }
+
+    @Test
+    public void numberSend() throws Exception {
+    }
+
+    @Test
+    public void restart() throws Exception {
+    }
+
+    @Test
+    public void messageSend() throws Exception {
+    }
+
+}
